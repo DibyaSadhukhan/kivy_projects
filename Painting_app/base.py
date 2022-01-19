d582d546c7b9e1ba1e3ac63129f273db92bdda2b
DibyaSadhukhan <60581408+DibyaSadhukhan@users.noreply.github.com>
2022-01-19 07:54:17 +0530

    The First Commit

--- Painting_app/base.py
+++ Painting_app/base.py
@@ -1,6 +1,8 @@
+#from playsound import playsound
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Color , Ellipse ,Line
+from kivy.uix.button import Button
+from kivy.graphics import Color , Line
 
 class Paint_Widget(Widget):
     def on_touch_down(self, touch):
@@ -8,11 +10,21 @@ class Paint_Widget(Widget):
             Color(1.,1.,0)
             touch.ud['line']=Line(points=(touch.x,touch.y))
     def on_touch_move(self, touch):
+        #playsound('sounds_tom.wav')
         touch.ud['line'].points += [touch.x,touch.y]
 
 class Paint_App(App):
     def build(self):
-        return Paint_Widget()
+        parent = Widget()
+        self.painter = Paint_Widget()
+        clearbtn = Button(text='Clear')
+        clearbtn.bind(on_release=self.clear_canvas)
+        parent.add_widget(self.painter)
+        parent.add_widget(clearbtn)
+        return parent
+    
+    def clear_canvas(self, obj):
+        self.painter.canvas.clear()
 
 if __name__=='__main__':
     Paint_App().run()
